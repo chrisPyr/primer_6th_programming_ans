8dbb01ae0d514927127433b9510a520a3218c475
chrisPyr <smile51293@gmail.com>
2021-10-17 22:11:25 +0800

    [CH2] 2_2 done

---
+++ CH2/2_2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  using namespace std;
+  cout << "Enter number of furlongs:\n";
+  double fur;
+  cin >> fur;
+  cout << fur << " furlongs equal to " << fur * 220 << " yards\n";
+  return 0;
+}
